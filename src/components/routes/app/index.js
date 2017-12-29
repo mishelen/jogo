1b0eaffe44febe876f68a2b7bae6d47cf176c079
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2017-12-29 12:09:12 +0300

    add other routes

--- src/components/routes/app/index.js
+++ src/components/routes/app/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import TopBar from './components/TopBar';
+import { ContactUs, Jogs, Info } from './routes';
 
 const App = () => (
     <div>
         <TopBar />
-        App
+        <Switch>
+            <Route path="/jogs" component={Jogs} />
+            <Route path="/info" component={Info} />
+            <Route path="/contact_us" component={ContactUs} />
+        </Switch>
     </div>
 );
 
---
+++ src/components/routes/app/routes/jogs/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => (
+    <article>
+        <ul>
+            <li>Lorem ipsum dolor sit.</li>
+            <li>Ad eaque porro quibusdam.</li>
+            <li>Modi molestiae obcaecati possimus?</li>
+            <li>Minus nulla ullam voluptatem.</li>
+            <li>Consequuntur dolorem et tempora.</li>
+        </ul>
+    </article>
+);
+
+export default NotFound;
