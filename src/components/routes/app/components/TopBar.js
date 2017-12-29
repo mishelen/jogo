d3d935b48f42e20fb60f13d8915e33d6f44842c0
Mikhail Hozhy <mikhail.hozhy@gmail.com>
2017-12-29 11:51:43 +0300

    add topBar

---
+++ src/components/routes/app/components/TopBar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { NavLink as Link } from 'react-router-dom';
+
+class TopBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <Navbar className="global-header" color="faded" dark expand="md">
+                <NavbarBrand href="/" title="YogoBear">
+                    <img src="./../../../../images/logo.svg" alt="logo" />
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink
+                                className="global-header__link"
+                                activeClassName="global-header__link--active"
+                                tag={Link}
+                                to="/jogs">
+                                Jogs
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className="global-header__link"
+                                activeClassName="global-header__link--active"
+                                tag={Link}
+                                to="/info">
+                                Info
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className="global-header__link"
+                                activeClassName="global-header__link--active"
+                                tag={Link}
+                                to="/contact_us">
+                                Contact Us
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default TopBar;
--- src/components/routes/app/index.js
+++ src/components/routes/app/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import TopBar from './components/TopBar';
 
-const App = () => <div>App</div>;
+const App = () => (
+    <div>
+        <TopBar />
+        App
+    </div>
+);
 
 export default App;
